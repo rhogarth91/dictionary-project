c2372670abd628fcb00aae5b32b7a84c45610fbe
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2025-09-13 15:52:59 +0100

    Improvement to image grid

--- src/Images.js
+++ src/Images.js
@@ -2,12 +2,19 @@ export default function Images(props) {
   console.log(props.images);
   if (props.images) {
     return (
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-6 md:mb-8">
+      <div className="row">
         {props.images.photos.map(function (image, index) {
           return (
-            <a href={image.src.original} target="_blank" rel="noreferrer">
-              <img src={image.src.landscape} alt={image.alt} key={index} />
-            </a>
+            <div className="col-4">
+              <a href={image.src.original} target="_blank" rel="noreferrer">
+                <img
+                  src={image.src.landscape}
+                  alt={image.alt}
+                  key={index}
+                  className="img-fluid"
+                />
+              </a>
+            </div>
           );
         })}
       </div>
